679fcc321248d8c46720ab5f11c4af4049aaab7b
CrazeKing <128724874+CrazeKing@users.noreply.github.com>
2023-04-02 14:08:52 +0530

    Added new Program

---
+++ SumOfSquares.py
@@ -0,0 +1,14 @@
+#Function declaration
+def squares_sum(numbers):
+    sum = 0
+    for num in numbers:
+        sum += num ** 2
+    return sum
+        
+numbers = [1, 2, 3, 4, 5, 6, 7]
+
+#Calling function
+result = squares_sum(numbers)
+
+#Output will be 140
+print("The Result is:",result)
