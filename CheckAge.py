dd594ecac47ded26070927fb3eb503d86916fcf8
CrazeKing <128724874+CrazeKing@users.noreply.github.com>
2023-04-02 14:03:17 +0530

    Added First Assignment

---
+++ CheckAge.py
@@ -0,0 +1,9 @@
+age = int(input("Please enter your Age: "))
+
+#Checking Age using if-elif-else statements
+if age < 18:
+    print("You are a Minor")
+elif age < 65:
+    print("You are an Adult")
+else:
+    print("You are a Senior")
